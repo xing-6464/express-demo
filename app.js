e334899032b468c0c14c059ae32049f72ff96575
xing-6464 <xing_6464@foxmail.com>
2023-04-19 18:35:50 +0800

    Express管理用户数据信息

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const fs = require('fs');
+const { promisify } = require('util');
+
+const readFile = promisify(fs.readFile);
 
 const app = express();
-
-app.get('/', (req, res) => {});
+app.get('/', async (req, res) => {
+  try {
+    const back = await readFile('./db.json', 'utf-8');
+    res.send(back);
+  } catch (e) {
+    res.status(500).json({ e });
+  }
+});
 
 app.listen(3000, () => {
   console.info('Run http://127.0.0.1:3000');
