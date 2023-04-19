0c8242fed5f9ab310e364baf4816ecc45044cfcf
xing-6464 <xing_6464@foxmail.com>
2023-04-19 18:42:58 +0800

    处理客户端 Post 请求数据

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const { promisify } = require('util');
 const readFile = promisify(fs.readFile);
 
 const app = express();
+app.use(express.urlencoded());
+app.use(express.json());
+
 app.get('/', async (req, res) => {
   try {
     const back = await readFile('./db.json', 'utf-8');
@@ -12,6 +15,10 @@ app.get('/', async (req, res) => {
   } catch (e) {
     res.status(500).json({ e });
   }
+});
+
+app.post('/', async (req, res) => {
+  console.info(req.body);
 });
 
 app.listen(3000, () => {
